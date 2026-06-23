e203668494384d61b99dc6f7960393d63bed92f0
marek-pozorantsi <marek.ipak@gmail.com>
2026-06-23 19:58:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='pozorantsi-song-base-v41-mobile-visual';
+const CACHE_NAME='pozorantsi-song-base-v42-admin-dot';
 const FILES=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
