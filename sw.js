18f8af783ce38a93f11c7dc71bf129c4f1b80077
marek-pozorantsi <marek.ipak@gmail.com>
2026-08-23 22:57:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
-const CACHE_NAME='pozorantsi-song-base-drum-view-1';
+const CACHE_NAME='pozorantsi-song-base-secure-rc4-3';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ e.respondWith(fetch(e.request).then(response=>{
+   if(response&&response.ok&&new URL(e.request.url).origin===self.location.origin){
+     const copy=response.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy)).catch(()=>{});
+   }
+   return response;
+ }).catch(()=>caches.match(e.request)));
+});
